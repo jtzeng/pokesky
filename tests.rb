24fff46a2cd996e4d907e367b92423377bae545e
Justin Z <megajustin@hotmail.com>
2014-03-23 01:26:29 +0800

    add b/w movesets

--- tests.rb
+++ tests.rb
@@ -1,18 +1,46 @@
 require './pokemon'
 require './generator'
 require './player'
+require './misc'
 
 module PokeSky
 
   # This doesn't actually test anything.
   def test
 
-    p Pokemon.xp_for_level(100)
-    p Pokemon.level_for_xp(pi.xp)
-    p pi.level
+    # This is pretty bad "testing" code. Oh well, it works.
+    # Check the Pokemon count.
+    puts @el.expr['movepools'].length == @el.expr['pokemon'].length
 
-    p = Player.new('Whac')
-    p p
+    mvpl_pkmn = @el.expr['movepools'].keys
+    all_pkmn = @el.expr['pokemon'].keys
+
+    # Check the Pokemon names.
+    puts all_pkmn == mvpl_pkmn
+    p all_pkmn.reject { |name| mvpl_pkmn.include?(name) }
+    p mvpl_pkmn.reject { |name| all_pkmn.include?(name) }
+
+    # Check the types.
+    puts @el.expr['types'] == @el.expr['attacks'].keys
+
+    # Check the number of attacks per type.
+    @el.expr['attacks'].each do |k, v|
+      print "#{v.length}, "
+    end
+    puts
+
+    # Check valid attacks.
+    valid_attacks = @el.expr['attacks'].values.flatten
+    @el.expr['movepools'].each do |k, v|
+      v.each do |mv|
+        if !valid_attacks.include?(mv)
+          puts "#{k}: #{mv}"
+        end
+      end
+    end
+
+    p Pokemon.xp_for_level(100)
+    p Pokemon.level_for_xp(Pokemon.xp_for_level(100))
 
     p [
        move_power('Aura Sphere'),
