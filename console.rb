0c36fe221154a5446ba90c2aa246b9686272d2be
Justin Z <megajustin@hotmail.com>
2014-03-23 14:21:36 +0800

    overload everything with colors

--- console.rb
+++ console.rb
@@ -19,14 +19,73 @@ require './player'
 
 module PokeSky
 
+  BLACK = "\033[30;1m"
+  RED = "\033[31;1m"
+  DARKRED = "\033[31m"
+  GREEN = "\033[32;1m"
+  DARKGREEN = "\033[32m"
+  YELLOW = "\033[33;1m"
+  BROWN = "\033[33m"
+  BLUE = "\033[34;1m"
+  DARKBLUE = "\033[34m"
+  MAGENTA = "\033[35;1m"
+  PURPLE = "\033[35m"
+  CYAN = "\033[36;1m"
+  DARKCYAN = "\033[36m"
+  WHITE = "\033[37;1m"
+  RESET = "\033[0m"
+
+  def color(type)
+    case type
+    when 'Bug'
+      GREEN
+    when 'Dark', 'Steel'
+      BLACK
+    when 'Dragon'
+      DARKBLUE
+    when 'Electric', 'Ground'
+      YELLOW
+    when 'Fighting', 'Rock'
+      BROWN
+    when 'Fire'
+      DARKRED
+    when 'Flying', 'Normal'
+      WHITE
+    when 'Ghost'
+      PURPLE
+    when 'Grass'
+      DARKGREEN
+    when 'Ice'
+      CYAN
+    when 'Poison', 'Psychic'
+      MAGENTA
+    when 'Water'
+      BLUE
+    else
+      DARKCYAN
+    end
+  end
+
+  def move_color(move)
+    "#{color(move_type(move))}#{move}#{RESET}"
+  end
+
+  def type_color(type)
+    "#{color(type)}#{type}#{RESET}"
+  end
+
+  def pkmn_color(pkmn)
+    "#{color(pkmn.type.prim)}#{pkmn.name}#{RESET}"
+  end
+
   def disp_party(plr)
     puts "#{plr.name}'s Party:"
     plr.party.each do |pkmn|
-      name = name_for_id(pkmn.id)
-      prim, sec = @el.expr['pokemon'][name]
-      moves = pkmn.moves.join(', ')
+      prim, sec = @el.expr['pokemon'][pkmn.name]
+      moves = pkmn.moves.map { |s| move_color(s) }.join(', ')
 
-      puts "#{name} (#{prim}#{sec ? ', ' << sec : ''}) - #{moves}"
+      s = sec ? ', ' << type_color(sec) : ''
+      puts "#{pkmn_color(pkmn)} (#{type_color(prim)}#{s}) - #{moves}"
     end
     puts
   end
@@ -35,12 +94,10 @@ module PokeSky
     if verbose
       puts "#{plr.name}'s Battlers:"
       plr.battlers.each_with_index do |pkmn, slot|
-        name = name_for_id(pkmn.id)
-
-        puts "[#{slot}]: #{name} (HP: #{pkmn.health})"
+        puts "[#{slot}]: #{pkmn_color(pkmn)} (HP: #{pkmn.health})"
       end
     else
-      battler_names = plr.battlers.map { |b| b.name }.join(', ')
+      battler_names = plr.battlers.map { |b| pkmn_color(b) }.join(', ')
       puts "#{plr.name}'s Battlers: #{battler_names}"
     end
   end
@@ -49,17 +106,17 @@ module PokeSky
   def effect_desc(atk_mul)
     case atk_mul
     when 0
-      'not effective'
+      "#{BLACK}not effective#{RESET}"
     when 0.25
-      'hardly effective'
+      "#{PURPLE}hardly effective#{RESET}"
     when 0.5
-      'not very effective'
+      "#{DARKGREEN}not very effective#{RESET}"
     when 1
-      'normally effective'
+      "#{WHITE}normally effective#{RESET}"
     when 2
-      'super effective'
+      "#{YELLOW}super effective#{RESET}"
     when 4
-      'ultra effective'
+      "#{RED}ultra effective#{RESET}"
     end
   end
 
@@ -87,21 +144,23 @@ module PokeSky
     # Start the battle loop.
     while true
 
+      sleep 1
+
       # These represent the Pokemon.
       attacker = offense.battlers[0]
       defender = defense.battlers[0]
 
-      puts "#{offense.name}'s #{attacker.name} (#{attacker.health} HP)"
-      puts "#{defense.name}'s #{defender.name} (#{defender.health} HP)"
-      puts "It's #{attacker.name}'s turn!"
+      print "#{offense.name}'s #{pkmn_color(attacker)} (#{attacker.health} HP) "
+      puts "vs #{defense.name}'s #{pkmn_color(defender)} (#{defender.health} HP)"
+      puts "It's #{pkmn_color(attacker)}'s turn!"
 
       forfeit = nil
       switch = nil
       move = nil
       if offense == plr
-        puts "Your #{attacker.name}'s moves:"
+        puts "Your #{pkmn_color(attacker)}'s moves:"
         attacker.moves.each_with_index do |m, slot|
-          puts "[#{slot}]: #{m}"
+          puts "[#{slot}]: #{move_color(m)}"
         end
 
         valid = false
@@ -144,14 +203,15 @@ module PokeSky
 
           break if valid
         end
-      else
-        sleep 1
       end
 
       break if forfeit
 
       if switch
-        puts "#{attacker.name}, return! Go, #{offense.battlers[switch].name}!"
+        old_name_c = pkmn_color(attacker)
+        new_name_c = pkmn_color(offense.battlers[switch])
+        puts "#{old_name_c}, return! Go, #{new_name_c}!"
+
         curr_id = offense.battlers.index(attacker)
         offense.battlers[switch], offense.battlers[curr_id] =
           offense.battlers[curr_id], offense.battlers[switch]
@@ -159,7 +219,10 @@ module PokeSky
         move ||= attacker.moves.sample
         hit = calculate_hit(move, attacker, defender)
 
-        puts "#{attacker.name}'s #{move} deals #{hit} damage to #{defender.name}!"
+        name_c = pkmn_color(attacker)
+        mv_c = move_color(move)
+        def_name_c = pkmn_color(defender)
+        puts "#{name_c}'s #{mv_c} deals #{hit} damage to #{def_name_c}!"
         effect = effect_desc(attack_multiplier(move_type(move),
                                                defender.type.prim,
                                                defender.type.sec))
@@ -167,7 +230,7 @@ module PokeSky
 
         defender.health -= hit
         if defender.health <= 0
-          puts "#{defense.name}'s #{defender.name} fainted!"
+          puts "#{defense.name}'s #{pkmn_color(defender)} fainted!"
           defense.battlers.delete(defender)
           disp_battlers(offense)
           disp_battlers(defense)
